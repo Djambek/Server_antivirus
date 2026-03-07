c322b0edfa3c3bcf9d03fa6e5c43d4cfd902a3e8
Djambek <d.d.tlepbergenov@gmail.com>
2026-03-07 09:55:39 +0300

    deleted unusage functionality

--- src/main/java/ru/mtuci/Repositories/AircraftRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.mtuci.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.mtuci.Entities.Aircraft;
-
-import java.util.UUID;
-
-public interface AircraftRepository extends JpaRepository<Aircraft, UUID> {
-}
--- src/main/java/ru/mtuci/Repositories/PassengerRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.mtuci.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.mtuci.Entities.Passenger;
-
-import java.util.UUID;
-
-public interface PassengerRepository extends JpaRepository<Passenger, UUID> {
-}
